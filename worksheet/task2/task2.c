62c3cdc05c8a6d526326d6516eb4044ad7663cbc
fizeenf <lhxz0885@leeds.ac.uk>
2026-03-24 17:38:15 +0000

    ..

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -20,20 +20,20 @@ int main(void) {
 
 	for(int i = 0; i < len; i++) {
 
-       char c = hex[i];
-       int value;
+        char c = hex[i];
+        int value;
 
 	// if input contains invalid hex digit
 	 
-	     if(c >= '0' && c <= '9') {
+	    if(c >= '0' && c <= '9') {
 
 		    value = c - '0';
 
-	     } else if (c >= 'A' && c <= 'F') {
+	    } else if (c >= 'A' && c <= 'F') {
 
 		    value = c - 'A' + 10;
 
-	     } else if (c >= 'a' && c <= 'f') {
+	    } else if (c >= 'a' && c <= 'f') {
 
 	        value = c-'a' + 10; 
 	    } else {
