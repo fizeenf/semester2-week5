695618f565eee3b7576a6490d32ff7f56164f609
fizeenf <lhxz0885@leeds.ac.uk>
2026-03-24 16:27:38 +0000

    wk 5

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -14,11 +14,32 @@ int main(void){
 	char hex[9];
 	
 	printf("Enter a hexadecimal:");
+    scanf("%s", hex); 
+
+	for(int i=0; i<strlen(hex) i++)
+
+       char c = hex[i];
+       int value;
 
 	// if input contains invalid hex digit
+    if(char>='0' && char<= '9') {
+		value = c - '0'
+	} else if (char>='A' && char<= 'F') {
+		value = c - 'A' + 10;
+
+	} else if (char>='a' && char<='f')
+	    value = c-'a' + 10; 
+	} else {
+		printf("Error: Invalid Hexadecimal\n");
+
+		return 0;
+	}
+
 	// printf("Error: Invalid Hexadecimal\n");
 	
 	// print the decimal result
+	decimal = decimal*16 + value;
+	
 	printf("decimal:%ld\n", decimal);
 	
 	return 0;
